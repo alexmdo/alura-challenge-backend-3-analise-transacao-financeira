36607eaf1e891ca8ba4bbcf4485e6ab2c19b0970
Alexandre Oliveira <alexmdo@gmail.com>
2022-09-14 21:55:41 -0300

    Enabling Bean Validation

--- src/main/java/br/com/alexmdo/txanalyser/AluraChallengeBackend3AnaliseTransacaoFinanceiraApplication.java
+++ src/main/java/br/com/alexmdo/txanalyser/AluraChallengeBackend3AnaliseTransacaoFinanceiraApplication.java
@@ -2,12 +2,26 @@ package br.com.alexmdo.txanalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+@Configuration
 @SpringBootApplication
 public class AluraChallengeBackend3AnaliseTransacaoFinanceiraApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AluraChallengeBackend3AnaliseTransacaoFinanceiraApplication.class, args);
+    }
+
+    @Bean
+    public Validator validator() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            return factory.getValidator();
+        }
     }
 
 }
